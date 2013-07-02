479d46dee306c4a3aed7ccf6ac795fd0f26b9d05
Marc Català <reskit@gmail.com>
2013-07-02 12:40:23 +0200

    Added instance lang strings

--- lang/ca/block_myremotecourses.php
+++ lang/ca/block_myremotecourses.php
@@ -6,3 +6,5 @@ $string['moodleold'] = 'Els cursos remots es troben sobre un Moodle 1.X';
 $string['myremoteurl'] = 'URL del Moodle remot';
 $string['myremoteurl_desc'] = 'URL a l\'arxiu ajax.php del Moodle remot';
 $string['errormyremotehost'] = 'S\'ha produït un error en recuperar els cursos remots. Si us plau intenti-ho més tard';
+$string['myremotecourses:addinstance'] = 'Afegeix un nou bloc Els meus cursos remots';
+$string['myremotecourses:myaddinstance'] = 'Afegeix un nou bloc Els meus cursos remots a La meva pàgina inicial';
--- lang/en/block_myremotecourses.php
+++ lang/en/block_myremotecourses.php
@@ -6,3 +6,5 @@ $string['moodleold'] = 'Moodle remote courses on Moodle 1.X';
 $string['myremoteurl'] = 'Remote Moodle URL';
 $string['myremoteurl_desc'] = 'URL to ajax.php file on remote Moodle';
 $string['errormyremotehost'] = 'Error while getting remote courses. Please try again later';
+$string['myremotecourses:addinstance'] = 'Add a new My remote courses block';
+$string['myremotecourses:myaddinstance'] = 'Add a new My remote courses block to My home';
